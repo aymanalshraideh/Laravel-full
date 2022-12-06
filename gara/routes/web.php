a342ce3bb9665c5006764585db63d0b2ec8c16e1
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-12-06 22:11:19 +0300

    main

---
+++ gara/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// All Listings
+Route::get('/', function () {
+    return view('listings', [
+        'headings' => 'latest listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+// Single Listing
+Route::get('listing/{listing}',function(Listing $listing){
+
+    return view('listing',[
+        
+        'listing' => $listing
+    ]);
+
+});
